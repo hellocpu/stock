1ac3fd246c2c19b04b0ccb412d72bbca5221550f
wangt <wangt@bsfit.cn>
2017-02-17 10:48:45 +0800

    DfpverifApi.java 兼容64位外码解码

--- src/main/java/com/bs/wt/controller/DfpverifApi.java
+++ src/main/java/com/bs/wt/controller/DfpverifApi.java
@@ -1,9 +1,9 @@
 package com.bs.wt.controller;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +17,8 @@ import cn.com.bsfit.dfp.validation.ValidateManager;
 @RequestMapping("/api")
 public class DfpverifApi {
 	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	// 公钥
 	static final String PK = "MIGWMA0GCSqGSIb3DQEBAQUAA4GEADCBgAJ5AL2KNNzht9zQVQPiXcpQGC0kd0aMUJscviBkaaGRL36U26r9UvG7yTVSYxLWRN7FU7eYy2J4otWLASk6atN8LEYkd5KQKuCn12Asp4T8JbJHi805sUvjrN5BfjHW+uN104CRwb/bK/2hHCcLmvRl5wu5MCRmbvZ8uwIDAQAB";
 	
@@ -24,7 +26,7 @@ public class DfpverifApi {
 	@RequestMapping(value="/transform",method = RequestMethod.GET)
     public String transformDfp(@RequestParam(value = "originStr") String originStr) throws IOException {
         // 输出
-        System.out.println("originStr: " + originStr);
+        log.debug("originStr : {}",originStr);
         if (originStr == null || originStr.isEmpty()) {
             return null;
         }
@@ -33,6 +35,9 @@ public class DfpverifApi {
         DeviceFingerPrint dfp = null;
         if (dfp == null) {
             try {
+            	if(originStr.length() < 160){
+            		return ValidateManager.decryptDFPInfo(originStr);
+            	}else
                 dfp=ValidateManager.decryptDFPInfo(external, PK);
             } catch (Exception e) {
                return "";
