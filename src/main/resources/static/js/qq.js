b5e1a8761c1488b7dfe233d318d13aaf1b0621ab
wangt <wangt@bsfit.cn>
2017-02-17 16:32:30 +0800

    add qq

---
+++ src/main/resources/static/js/qq.js
@@ -0,0 +1,19 @@
+$(function(){
+	var thisBox = $('.lanren');
+	var defaultTop = thisBox.offset().top;
+	var slide_min = $('.lanren .slide_min');
+	var slide_box = $('.lanren .slide_box');
+	var closed = $('.lanren .slide_box h2 img');
+	slide_min.on('click',function(){$(this).hide();	slide_box.show();});
+	closed.on('click',function(){slide_box.hide().hide();slide_min.show();});
+	// 页面滚动的同时，悬浮框也跟着滚动
+	$(window).on('scroll',function(){scro();});
+	$(window).onload = scro();
+	function scro(){
+		var offsetTop = defaultTop + $(window).scrollTop()+'px';
+		thisBox.animate({top:offsetTop},
+		{	duration: 600,	//滑动速度
+	     	queue: false    //此动画将不进入动画队列
+	     });
+	}
+});
